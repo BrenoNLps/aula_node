713c3888a67ac9b379ed9501b55fcbff09b871d4
brenonlps <brenolopes.xy@gmail.com>
2026-05-07 13:43:57 -0300

    Refactor Tarefa model: move definition to Tarefa.ts and update import case

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sequelize = require('./db')
 const server = require('./server')
-require('./models/Tarefa') 
+require('./models/tarefa') 
 
 sequelize.sync().then(() => {
     server.listen(process.env.PORT || 3000, () => {
