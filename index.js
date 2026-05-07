e5978670ff4617d81abedf9809b2b33c7103c1c0
brenonlps <brenolopes.xy@gmail.com>
2026-05-07 13:36:39 -0300

    Add Tarefa model import to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sequelize = require('./db')
 const server = require('./server')
+require('./models/tarefa') 
 
 sequelize.sync().then(() => {
     server.listen(process.env.PORT || 3000, () => {
