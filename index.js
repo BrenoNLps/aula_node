45c630c8d216334855f7fa97ee1b81bfc41f251d
brenonlps <brenolopes.xy@gmail.com>
2026-05-07 14:23:24 -0300

    Remove Tarefa model and add Usuario model definition

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sequelize = require('./db')
 const server = require('./server')
-require('./models/Tarefa')
+require('./models/Usuario')
 
 sequelize.sync().then(() => {
     server.listen(process.env.PORT || 3000, () => {
--- models/Tarefa.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../db')
-
-const Tarefa = sequelize.define('Tarefa', {
-    titulo: { type: DataTypes.STRING, allowNull: false },
-    concluida: { type: DataTypes.BOOLEAN, defaultValue: false }
-})
-
-module.exports = Tarefa
